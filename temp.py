ee2a2f6ee5b9767e5978dfc885b0fe8ed3f799ef
E5Anant <e5anant2011@gmail.com>
2024-10-10 14:36:49 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,6 +0,0 @@
-from archeai import Tool
-from archeai.llms import Gemini
-from archeai.tools import web_search
-
-hehe = Tool(func=web_search, description="hehe",returns_value=True, verbose=True)
-print(hehe.params)

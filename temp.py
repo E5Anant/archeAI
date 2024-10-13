6fbfebb2e0575f8832856bd1e51b6f3c43579f44
E5Anant <e5anant2011@gmail.com>
2024-10-13 23:59:39 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-from archeai import Tool
-from archeai.llms import Gemini
-from archeai.tools import web_search
-
-hehe = Tool(func=web_search, description="hehe",returns_value=True, verbose=True)
-print(hehe.params)
-
-## Task : Get the current time and what should we do at this time and write the article in a file time.txt.
